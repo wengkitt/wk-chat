d551e32894f003c6a13d7c6da066c08eb3631128
Tang Weng Kitt <wengkitt10@gmail.com>
2025-06-18 22:23:37 +0800

    Add convex as backend

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./style.css";
 import { RouterProvider } from "react-router";
 import { router } from "./router";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ConvexProvider client={convex}>
+      <RouterProvider router={router} />
+    </ConvexProvider>
   </StrictMode>
 );
