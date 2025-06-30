739f58a79d3a190281f58c1b1ca84cc437ecc74f
Tang Weng Kitt <wengkitt10@gmail.com>
2025-07-01 07:16:38 +0800

    Add convex

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as LayoutWrapper } from "./layout-wrapper";
 export { default as WelcomeScreen } from "./welcome-screen";
 export { default as ChatInput } from "./chat-input";
 export { default as ChatMessage } from "./chat-message";
+export { default as ConvexClientProvider } from "./convex-client-provider";
